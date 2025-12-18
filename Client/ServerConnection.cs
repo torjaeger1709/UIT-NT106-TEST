274d378d479bccc0c035248f98901baad2420d55
maikoakoa <24520817@gm.uit.edu.vn>
2025-12-18 16:09:32 +0700

    xong giao diện

---
+++ Client/ServerConnection.cs
@@ -0,0 +1,164 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public enum PacketType : int
+    {
+        GET_MENU = 1, ORDER = 2, GET_ORDERS = 3, PAY = 4, QUIT = 99
+    }
+
+    public class MonAn
+    {
+        public int ID { get; set; }
+        public string TenMon { get; set; }
+        public decimal DonGia { get; set; }
+    }
+
+    public class OrderDetailDTO
+    {
+        public int Ban { get; set; }
+        public string TenMon { get; set; }
+        public int SL { get; set; }
+        public decimal ThanhTien { get; set; }
+    }
+
+    public class ServerConnection
+    {
+        private TcpClient _client;
+        private NetworkStream _stream;
+        private BinaryReader _reader;
+        private BinaryWriter _writer;
+
+        public bool IsConnected => _client != null && _client.Connected;
+
+        public bool Connect(string ip, int port)
+        {
+            try
+            {
+                if (IsConnected) return true;
+                _client = new TcpClient();
+                _client.Connect(ip, port);
+                _stream = _client.GetStream();
+                _reader = new BinaryReader(_stream);
+                _writer = new BinaryWriter(_stream);
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                if (IsConnected) SendPacket(PacketType.QUIT, null);
+                _client?.Close();
+            }
+            catch { }
+        }
+
+        // Hàm gửi nhận gói tin
+        private BinaryReader SendPacket(PacketType type, Action<BinaryWriter> writePayloadAction)
+        {
+            if (!IsConnected) throw new Exception("Chưa kết nối Server");
+
+            try
+            {
+                // Đóng gói Payload
+                using (MemoryStream ms = new MemoryStream())
+                using (BinaryWriter tempWriter = new BinaryWriter(ms))
+                {
+                    tempWriter.Write((int)type);
+                    writePayloadAction?.Invoke(tempWriter);
+                    byte[] payload = ms.ToArray();
+
+                    // Gửi Header (Length) + Payload
+                    _writer.Write(payload.Length);
+                    _writer.Write(payload);
+                    _writer.Flush();
+                }
+
+                // Nhận phản hồi (nếu không phải lệnh QUIT)
+                if (type == PacketType.QUIT) return null;
+
+                int respLength = _reader.ReadInt32();
+                byte[] respData = _reader.ReadBytes(respLength);
+                return new BinaryReader(new MemoryStream(respData));
+            }
+            catch (Exception ex)
+            {
+                _client?.Close();
+                throw new Exception("Lỗi mạng: " + ex.Message);
+            }
+        }
+
+        // Các hàm xử lý khi nhận từ server
+
+        public List<MonAn> GetMenu()
+        {
+            try
+            {
+                using (BinaryReader resp = SendPacket(PacketType.GET_MENU, null))
+                {
+                    bool success = resp.ReadBoolean();
+                    string json = resp.ReadString();
+                    if (success)
+                        return JsonConvert.DeserializeObject<List<MonAn>>(json);
+                }
+            }
+            catch { }
+            return new List<MonAn>();
+        }
+
+        public (bool success, string msg) Order(int ban, int idMon, int sl)
+        {
+            try
+            {
+                using (BinaryReader resp = SendPacket(PacketType.ORDER, (bw) => {
+                    bw.Write(ban);
+                    bw.Write(idMon);
+                    bw.Write(sl);
+                }))
+                {
+                    return (resp.ReadBoolean(), resp.ReadString());
+                }
+            }
+            catch (Exception ex) { return (false, ex.Message); }
+        }
+
+        public List<OrderDetailDTO> GetOrders()
+        {
+            try
+            {
+                using (BinaryReader resp = SendPacket(PacketType.GET_ORDERS, null))
+                {
+                    if (resp.ReadBoolean())
+                    {
+                        string json = resp.ReadString();
+                        return JsonConvert.DeserializeObject<List<OrderDetailDTO>>(json);
+                    }
+                }
+            }
+            catch { }
+            return new List<OrderDetailDTO>();
+        }
+
+        public (bool success, string msg, decimal total) Pay(int ban)
+        {
+            try
+            {
+                using (BinaryReader resp = SendPacket(PacketType.PAY, (bw) => bw.Write(ban)))
+                {
+                    bool success = resp.ReadBoolean();
+                    string msg = resp.ReadString();
+                    decimal total = resp.ReadDecimal();
+                    return (success, msg, total);
+                }
+            }
+            catch (Exception ex) { return (false, ex.Message, 0); }
+        }
+    }
+}
---
+++ Client/ThuNgan.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Client
+{
+    partial class ThuNgan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(776, 307);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(661, 367);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Charge";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(119, 359);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 362);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Số bàn";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(410, 367);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Tổng tiền";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(503, 367);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 5;
+            label3.Text = "(tổng)";
+            // 
+            // ThuNgan
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "ThuNgan";
+            Text = "ThuNgan";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
