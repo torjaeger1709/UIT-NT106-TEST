2693faef66bf644abc70477a44d4a9c8ea0d44c8
torjaeger1709 <nguyendkhiem1709@gmail.com>
2025-12-18 16:12:04 +0700

    them form cho server

--- Server/Server.Designer.cs
+++ Server/Server.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbLog.Location = new System.Drawing.Point(0, 58);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(800, 392);
+            this.rtbLog.TabIndex = 4;
+            this.rtbLog.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblPort);
+            this.panel1.Controls.Add(this.lblIP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 58);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.Location = new System.Drawing.Point(643, 26);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(45, 20);
+            this.lblPort.TabIndex = 1;
+            this.lblPort.Text = "Port:";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIP.Location = new System.Drawing.Point(36, 26);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(29, 20);
+            this.lblIP.TabIndex = 0;
+            this.lblIP.Text = "IP:";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.panel1);
+            this.Name = "Server";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblIP;
     }
 }
 
